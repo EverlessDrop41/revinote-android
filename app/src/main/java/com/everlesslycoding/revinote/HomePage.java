b00ec770e39fa9e8a151ff2664a96a9dc96b7fc3
Emily Peregrine <tomfperegrine@googlemail.com>
2015-10-18 23:51:03 +0100

    Can now switch to settings page

--- app/src/main/java/com/everlesslycoding/revinote/HomePage.java
+++ app/src/main/java/com/everlesslycoding/revinote/HomePage.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,6 +30,14 @@ public class HomePage extends AppCompatActivity {
         Firebase ref = new Firebase("https://revinote.firebaseio.com/");
 
         NameLabel.setText(ref.getAuth().getProviderData().get("email").toString());
+
+        SettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomePage.this, UserSettings.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
