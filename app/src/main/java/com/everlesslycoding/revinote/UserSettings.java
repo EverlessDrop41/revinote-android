cc1c7ab471886c738a3df39597631db076e01afc
Emily Peregrine <tomfperegrine@googlemail.com>
2015-10-26 02:06:42 +0000

    Implemented functionality for UserSetting menubar

--- app/src/main/java/com/everlesslycoding/revinote/UserSettings.java
+++ app/src/main/java/com/everlesslycoding/revinote/UserSettings.java
@@ -142,7 +142,8 @@ public class UserSettings extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_home) {
+            finish();
             return true;
         }
 
