c395aa8a0f405d4646eff80783baff7befe1491d
emilyperegrine <tomfperegrine@googlemail.com>
2015-11-11 21:15:47 +0000

    Added catch for startup error

--- app/src/main/java/com/everlesslycoding/revinote/MainActivity.java
+++ app/src/main/java/com/everlesslycoding/revinote/MainActivity.java
@@ -42,95 +42,99 @@ public class MainActivity extends AppCompatActivity {
         rootRef = new Firebase("https://revinote.firebaseio.com/");
 
         AuthData authData = rootRef.getAuth();
+        try {
+            if (authData != null && true) {
+                LoadHomePage();
+            } else {
+                EmailInput = (EditText)findViewById(R.id.EmailInput);
+                PasswordInput = (EditText)findViewById(R.id.PasswordInput);
+                LoginButton = (Button)findViewById(R.id.LoginButton);
+                ForgotPassBtn = (Button) findViewById(R.id.ForgotPassword);
+                SignUpButton = (Button) findViewById(R.id.SignUpButton);
 
-        if (authData != null && true) {
-            LoadHomePage();
-        } else {
-            EmailInput = (EditText)findViewById(R.id.EmailInput);
-            PasswordInput = (EditText)findViewById(R.id.PasswordInput);
-            LoginButton = (Button)findViewById(R.id.LoginButton);
-            ForgotPassBtn = (Button) findViewById(R.id.ForgotPassword);
-            SignUpButton = (Button) findViewById(R.id.SignUpButton);
+                ButtonLayout = (LinearLayout) findViewById(R.id.buttonsLayout);
+                LoadingBar = (ProgressBar) findViewById(R.id.progressBar);
 
-            ButtonLayout = (LinearLayout) findViewById(R.id.buttonsLayout);
-            LoadingBar = (ProgressBar) findViewById(R.id.progressBar);
+                hideProgressBar();
 
-            hideProgressBar();
+                LoginButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final String Email = EmailInput.getText().toString();
+                        final String Password = PasswordInput.getText().toString();
 
-            LoginButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    final String Email = EmailInput.getText().toString();
-                    final String Password = PasswordInput.getText().toString();
+                        showProgressBar();
 
-                    showProgressBar();
+                        Firebase.AuthResultHandler LoginHandler = new Firebase.AuthResultHandler() {
+                            @Override
+                            public void onAuthenticated(AuthData authData) {
+                                hideProgressBar();
+                                // Authenticated successfully with payload authData
+                                Toast.makeText(getBaseContext(), (String) authData.getProviderData().get("email"), Toast.LENGTH_LONG).show();
+                                LoadHomePage();
+                            }
 
-                    Firebase.AuthResultHandler LoginHandler = new Firebase.AuthResultHandler() {
-                        @Override
-                        public void onAuthenticated(AuthData authData) {
-                            hideProgressBar();
-                            // Authenticated successfully with payload authData
-                            Toast.makeText(getBaseContext(), (String) authData.getProviderData().get("email"), Toast.LENGTH_LONG).show();
-                            LoadHomePage();
-                        }
+                            @Override
+                            public void onAuthenticationError(FirebaseError error) {
+                                hideProgressBar();
+                                Toast.makeText(getBaseContext(), "[LI] ERROR: " + error.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        };
+                        Log.d("LOGIN DATA", "Email: " + Email + " Password: " + Password);
+                        rootRef.authWithPassword(Email, Password, LoginHandler);
+                    }
+                });
 
-                        @Override
-                        public void onAuthenticationError(FirebaseError error) {
-                            hideProgressBar();
-                            Toast.makeText(getBaseContext(), "[LI] ERROR: " + error.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    };
-                    Log.d("LOGIN DATA", "Email: " + Email + " Password: " + Password);
-                    rootRef.authWithPassword(Email, Password, LoginHandler);
-                }
-            });
+                SignUpButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final String Email = EmailInput.getText().toString();
+                        final String Password = PasswordInput.getText().toString();
 
-            SignUpButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    final String Email = EmailInput.getText().toString();
-                    final String Password = PasswordInput.getText().toString();
+                        showProgressBar();
 
-                    showProgressBar();
+                        final Firebase.ResultHandler SignUpHandler = new Firebase.ResultHandler() {
+                            @Override
+                            public void onSuccess() {
+                                rootRef.authWithPassword(Email, Password, new Firebase.AuthResultHandler() {
+                                    @Override
+                                    public void onAuthenticated(AuthData authData) {
+                                        // Authenticated successfully with payload authData
+                                        Toast.makeText(getBaseContext(), (String) authData.getProviderData().get("email"), Toast.LENGTH_LONG).show();
+                                        hideProgressBar();
+                                        LoadHomePage();
+                                    }
 
-                    final Firebase.ResultHandler SignUpHandler = new Firebase.ResultHandler() {
-                        @Override
-                        public void onSuccess() {
-                            rootRef.authWithPassword(Email, Password, new Firebase.AuthResultHandler() {
-                                @Override
-                                public void onAuthenticated(AuthData authData) {
-                                    // Authenticated successfully with payload authData
-                                    Toast.makeText(getBaseContext(), (String) authData.getProviderData().get("email"), Toast.LENGTH_LONG).show();
-                                    hideProgressBar();
-                                    LoadHomePage();
-                                }
+                                    @Override
+                                    public void onAuthenticationError(FirebaseError error) {
+                                        hideProgressBar();
+                                        Toast.makeText(getBaseContext(), "[SU-LI] ERROR: " + error.getMessage() + " " + Email, Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
 
-                                @Override
-                                public void onAuthenticationError(FirebaseError error) {
-                                    hideProgressBar();
-                                    Toast.makeText(getBaseContext(), "[SU-LI] ERROR: " + error.getMessage() + " " + Email, Toast.LENGTH_LONG).show();
-                                }
-                            });
-                        }
+                            @Override
+                            public void onError(FirebaseError error) {
+                                hideProgressBar();
+                                Toast.makeText(getBaseContext(), "[SU] ERROR: " + error.getMessage() + " " + Email, Toast.LENGTH_LONG).show();
+                            }
+                        };
 
-                        @Override
-                        public void onError(FirebaseError error) {
-                            hideProgressBar();
-                            Toast.makeText(getBaseContext(), "[SU] ERROR: " + error.getMessage() + " " + Email, Toast.LENGTH_LONG).show();
-                        }
-                    };
+                        rootRef.createUser(Email, Password, SignUpHandler);
+                    }
+                });
 
-                    rootRef.createUser(Email, Password, SignUpHandler);
-                }
-            });
-
-            ForgotPassBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(MainActivity.this, ForgotPassword.class);
-                    startActivity(i);
-                }
-            });
+                ForgotPassBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this, ForgotPassword.class);
+                        startActivity(i);
+                    }
+                });
+            }
+        } catch (NullPointerException npe) {
+            npe.printStackTrace();
+            recreate();
         }
     }
 
