87a411c85057fbd207eb840823af1d60bca93ec0
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-11-13 12:29:29 +0000

    Added subject class

---
+++ app/src/main/java/com/everlesslycoding/revinote/Subjects/Subject.java
@@ -0,0 +1,34 @@
+package com.everlesslycoding.revinote.Subjects;
+
+/**
+ * Created by Tom on 13/11/2015.
+ */
+public class Subject {
+    String name;
+    String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Subject () {
+        //Empty for firebase
+    }
+
+    public Subject(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
