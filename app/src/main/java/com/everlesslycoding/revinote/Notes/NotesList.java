b06d9556f62f1a3a5e7c55e117aca3af61cf85b5
emilyperegrine <tomfperegrine@googlemail.com>
2015-11-15 22:18:40 +0000

    Setup notes list menu

--- app/src/main/java/com/everlesslycoding/revinote/Notes/NotesList.java
+++ app/src/main/java/com/everlesslycoding/revinote/Notes/NotesList.java
@@ -2,6 +2,8 @@ package com.everlesslycoding.revinote.Notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.everlesslycoding.revinote.R;
 
@@ -11,5 +13,24 @@ public class NotesList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_notes_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
